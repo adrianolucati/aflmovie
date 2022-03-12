31ec54f87524515b35563b5e231a69c5420ab4ff
Adriano Lucati <adrianolucati@outlook.com>
2022-03-12 09:44:07 -0300

    Pagination

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,15 +1,21 @@
 import { ReactComponent as Arrow } from '../../assets/img/arrow.svg'
+import { MoviePage } from '../../types/movie';
 import './styles.css'
 
-function Pagination() {
+type Props = {
+    page: MoviePage;
+    onChange: Function;
+}
+
+function Pagination({ page, onChange } : Props) {
     return (
         <div className="aflmovie-pagination-container">
             <div className="aflmovie-pagination-box">
-                <button className="aflmovie-pagination-button" disabled={true} >
+                <button className="aflmovie-pagination-button" disabled={page.first} onClick={() => onChange(page.number -1)} >
                     <Arrow />
                 </button>
-                <p>{`${1} de ${3}`}</p>
-                <button className="aflmovie-pagination-button" disabled={false} >
+                <p>{`${page.number + 1} de ${page.totalPages}`}</p>
+                <button className="aflmovie-pagination-button" disabled={page.last} onClick={() => onChange(page.number + 1)}>
                     <Arrow className="aflmovie-flip-horizontal" />
                 </button>
             </div>
